b0735ccafa002b98d62e27a98955218c4f40c3a3
Surafel Teshome <lferasu@yahoo.com>
2025-02-16 11:27:39 -0500

    sidepanel config and start

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,18 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log('Hello Achiver', { id: browser.runtime.id });
+  try {
+    chrome.sidePanel
+      .setPanelBehavior({ openPanelOnActionClick: true })
+      .catch((error) => console.error(error));
+
+    chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+      await chrome.sidePanel.setOptions({
+        tabId,
+        path: 'popup.html',
+        enabled: true,
+      })
+    })
+  } catch (e) {
+    console.log("error is :", e)
+  }
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest : {
+    permissions : ['tabs', 'scripting', 'sidePanel'],
+    background: { "service_worker": "background.js" },
+    side_panel : {
+      default_path : "popup.html"
+    }
+  },
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
 });
